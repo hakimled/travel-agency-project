aaf89eaf4e17e75b9f6c21175b36656da5697122
hakimled <hakimled30@gmail.com>
2024-11-26 23:09:16 +0100

    changing background color randomly using a javascript function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const h1El = document.querySelector('h1');
+const divEl = document.querySelector('.container');
 
 function changeColor(){
     h1El.addEventListener('click', function(){
@@ -6,3 +7,21 @@ function changeColor(){
     })
 }
 changeColor();
+
+
+function randomColor(){
+    strings = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+    let hexColor  = '#' ;
+    for (let i = 0 ; i <6 ; i++){
+        let randomEl = Math.floor(Math.random() * strings.length);
+        hexColor += strings[randomEl];
+        
+
+    }
+    console.log(hexColor);
+    divEl.style.backgroundColor = hexColor;
+
+
+    
+}
+randomColor();
