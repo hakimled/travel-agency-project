c240ccabd78d10164a677c02ae863e82cf821132
hakimled <hakimled30@gmail.com>
2024-11-28 06:53:08 +0100

    script.js file is modified

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ function changeColor(){
 }
 changeColor();
 
-
 function randomColor(){
     strings = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
-    let hexColor  = '#' ;
-    for (let i = 0 ; i <6 ; i++){
-        let randomEl = Math.floor(Math.random() * strings.length);
-        hexColor += strings[randomEl];
-        
+    window.addEventListener('click', function(){
+        let hexColor  = '#' ;
+        for (let i = 0 ; i <6 ; i++){
+            let randomEl = Math.floor(Math.random() * strings.length);
+            hexColor += strings[randomEl];
+        }
+        console.log(hexColor);
+        divEl.style.backgroundColor = hexColor; 
 
-    }
-    console.log(hexColor);
-    divEl.style.backgroundColor = hexColor;
+    });
+   
 
-
-    
 }
+
 randomColor();
